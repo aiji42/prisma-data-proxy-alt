661464ef08c85eff5902585fbaf5ef8b120bbf5c
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-06-10 01:21:37 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
-import { PrismaClient } from '@prisma/client'
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import { PrismaClient, Prisma } from "@prisma/client";
+
+console.log(Prisma.dmmf);
 
 const db = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-})
+  log: ["query", "info", "warn", "error"],
+});
 
 const typeDefs = gql`
   type User {
@@ -32,26 +34,65 @@ const typeDefs = gql`
   }
 `;
 
-const resolvers = {
-  User: {
-    Team: async (_: unknown, args: any, context: { promise: any }) => {
-      return context.promise = context.promise.Team(args)
-    },
-  },
-  Team: {
-    users: async (_: unknown, args: any, context: { promise: any }) => {
-      return context.promise = context.promise.users(args)
-    },
-  },
-  Query: {
-    findFirstUser: async (_: unknown, args: any, context: { promise: any }) => {
-      return context.promise = db.user.findFirst(args)
-    },
-    findFirstTeam: async (_: unknown, args: any, context: { promise: any }) => {
-      return context.promise = db.team.findFirst(args)
-    },
-  },
+const toLowerFirstLetter = (str: string) => {
+  return str.charAt(0).toLowerCase() + str.substring(1);
 };
+
+const relatedRecordProxy = (fields: string[]) => {
+  return new Proxy(
+    {},
+    {
+      get: (_, method) => {
+        if (!fields.includes(method.toString())) return undefined;
+        return async (_: unknown, args: any, context: { promise: any }) => {
+          return (context.promise = context.promise[method](args));
+        };
+      },
+    }
+  );
+};
+
+const resolvers = {
+  User: relatedRecordProxy(["Team"]),
+  Team: relatedRecordProxy(["users"]),
+  Query: new Proxy(
+    {},
+    {
+      get: (_, method) => {
+        return async (_: unknown, args: any, context: { promise: any }) => {
+          if (method.toString().startsWith("findFirst")) {
+            return (context.promise =
+              // @ts-ignore
+              db[
+                toLowerFirstLetter(method.toString().replace(/^findFirst/, ""))
+              ].findFirst(args));
+          }
+        };
+      },
+    }
+  ),
+};
+
+// const resolvers = {
+//   User: {
+//     Team: async (_: unknown, args: any, context: { promise: any }) => {
+//       return (context.promise = context.promise.Team(args));
+//     },
+//   },
+//   Team: {
+//     users: async (_: unknown, args: any, context: { promise: any }) => {
+//       return (context.promise = context.promise.users(args));
+//     },
+//   },
+//   Query: {
+//     findFirstUser: async (_: unknown, args: any, context: { promise: any }) => {
+//       return (context.promise = db.user.findFirst(args));
+//     },
+//     findFirstTeam: async (_: unknown, args: any, context: { promise: any }) => {
+//       return (context.promise = db.team.findFirst(args));
+//     },
+//   },
+// };
 
 const port = process.env.PORT || "3000";
 const corsOrigin = process.env.CORS_ORIGIN;
@@ -60,11 +101,13 @@ const corsOrigin = process.env.CORS_ORIGIN;
   const app = express();
   const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
   });
   await server.start();
-  server.applyMiddleware({ app, path: '/*' });
+  server.applyMiddleware({ app, path: "/*" });
   app.listen({ port }, () =>
-    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+    console.log(
+      `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+    )
   );
-})()
+})();
