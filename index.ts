a55f02c66ce56c7913b0d1cdeb6b88d1f3a483ba
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-06-12 22:33:11 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,103 +1,148 @@
+import "reflect-metadata";
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
 import { PrismaClient, Prisma } from "@prisma/client";
 
-console.log(Prisma.dmmf);
+const TypeDefinitions = Prisma.dmmf.schema.outputObjectTypes.model.map((m) => {
+  return `
+      type ${m.name} {
+        ${m.fields
+          .map((f) => {
+            return `${f.name}${
+              f.args.length > 0
+                ? `(${f.args.map(({ name }) => `${name}: Any`).join(" ")})`
+                : ""
+            }: ${
+              f.outputType.isList
+                ? `[${f.outputType.type}]`
+                : `${f.outputType.type}`
+            }${f.isNullable ? "!" : ""}`;
+          })
+          .join(" ")}
+      }
+      `;
+});
+
+const Definitions = Prisma.dmmf.schema.outputObjectTypes.prisma.map((m) => {
+  return `
+      type ${m.name} {
+        ${m.fields
+          .map((f) => {
+            return `${f.name}${
+              f.args.length > 0
+                ? `(${f.args.map(({ name }) => `${name}: Any`).join(" ")})`
+                : ""
+            }: ${
+              f.outputType.isList
+                ? `[${f.outputType.type}]`
+                : `${f.outputType.type}`
+            }${f.isNullable ? "!" : ""}`;
+          })
+          .join(" ")}
+      }
+      `;
+});
 
 const db = new PrismaClient({
   log: ["query", "info", "warn", "error"],
 });
 
 const typeDefs = gql`
-  type User {
-    id: Int
-    name: String
-    email: String
-    teamId: String
-    createdAt: String
-    updatedAt: String
-    Team(where: ANY): Team
-  }
-  type Team {
-    id: ID
-    name: String
-    labels: [String]
-    createdAt: String
-    updatedAt: String
-    users(where: ANY): [User]
-  }
-  scalar ANY
+  scalar Any
+  scalar DateTime
+  scalar Json
 
-  type Query {
-    findFirstUser(where: ANY): User
-    findFirstTeam(where: ANY): Team
-  }
+  ${TypeDefinitions}
+
+  ${Definitions}
 `;
 
 const toLowerFirstLetter = (str: string) => {
   return str.charAt(0).toLowerCase() + str.substring(1);
 };
 
-const relatedRecordProxy = (fields: string[]) => {
+const relatedRecordProxy = (model: string) => {
   return new Proxy(
     {},
     {
       get: (_, method) => {
-        if (!fields.includes(method.toString())) return undefined;
-        return async (_: unknown, args: any, context: { promise: any }) => {
-          return (context.promise = context.promise[method](args));
+        const { fields } =
+          Prisma.dmmf.datamodel.models.find(({ name }) => name === model) ?? {};
+        if (!fields) return undefined;
+        const idFieldName = fields.find(({ isId }) => isId)?.name;
+        const field = fields.find(({ name }) => name === method.toString());
+        if (!field?.relationName) return undefined;
+
+        return async (
+          parent: unknown,
+          args: any,
+          context: { promise: any }
+        ) => {
+          try {
+            // @ts-ignore
+            return await db[toLowerFirstLetter(model)]
+              // @ts-ignore
+              .findUnique({ where: { [idFieldName]: parent[idFieldName] } })
+              [method](args);
+          } catch (e) {
+            console.log(e);
+          }
         };
       },
     }
   );
 };
 
+const mapping: Record<string, { operation: string; model: string }> =
+  Object.fromEntries(
+    Prisma.dmmf.mappings.modelOperations.flatMap(
+      ({ model, plural, ...operations }) => {
+        return Object.entries(operations).map(([k, v]) => [
+          v,
+          { operation: k, model },
+        ]);
+      }
+    )
+  );
+
 const resolvers = {
-  User: relatedRecordProxy(["Team"]),
-  Team: relatedRecordProxy(["users"]),
+  User: relatedRecordProxy("User"),
+  Team: relatedRecordProxy("Team"),
   Query: new Proxy(
     {},
     {
       get: (_, method) => {
         return async (_: unknown, args: any, context: { promise: any }) => {
-          if (method.toString().startsWith("findFirst")) {
-            return (context.promise =
-              // @ts-ignore
-              db[
-                toLowerFirstLetter(method.toString().replace(/^findFirst/, ""))
-              ].findFirst(args));
-          }
+          const { operation, model } = mapping[method.toString()];
+          console.log(args, operation);
+
+          return (context.promise =
+            // @ts-ignore
+            db[toLowerFirstLetter(model)][operation](args));
+        };
+      },
+    }
+  ),
+  Mutation: new Proxy(
+    {},
+    {
+      get: (_, method) => {
+        return async (_: unknown, args: any, context: { promise: any }) => {
+          const { operation, model } = mapping[method.toString()];
+          return (context.promise =
+            // @ts-ignore
+            db[toLowerFirstLetter(model)][operation](args));
         };
       },
     }
   ),
 };
 
-// const resolvers = {
-//   User: {
-//     Team: async (_: unknown, args: any, context: { promise: any }) => {
-//       return (context.promise = context.promise.Team(args));
-//     },
-//   },
-//   Team: {
-//     users: async (_: unknown, args: any, context: { promise: any }) => {
-//       return (context.promise = context.promise.users(args));
-//     },
-//   },
-//   Query: {
-//     findFirstUser: async (_: unknown, args: any, context: { promise: any }) => {
-//       return (context.promise = db.user.findFirst(args));
-//     },
-//     findFirstTeam: async (_: unknown, args: any, context: { promise: any }) => {
-//       return (context.promise = db.team.findFirst(args));
-//     },
-//   },
-// };
-
 const port = process.env.PORT || "3000";
 const corsOrigin = process.env.CORS_ORIGIN;
 
 (async () => {
+  db.$connect();
   const app = express();
   const server = new ApolloServer({
     typeDefs,
--- sample.ts
+++ sample.ts
@@ -1,18 +1,27 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const db = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-})
-
+  log: ["query", "info", "warn", "error"],
+});
 
 const main = async () => {
   const res = await Promise.all([
-    db.user.findFirst({ where: { id: 1 }, select: { id: true, Team: true } }),
-    db.team.findFirst({ select: { id: true, users: { where: { id: 1 } } } }),
-  ])
-  console.log(res)
-}
+    db.user.findFirst({
+      where: { id: { equals: 1 } },
+      select: { id: true, Team: true },
+    }),
+    db.team.findFirst({
+      select: { id: true, users: { where: { id: { equals: 1 } } } },
+    }),
+    db.team.findMany({
+      where: { name: { contains: "t", mode: "insensitive" } },
+      select: { users: { where: { id: 1 } } },
+    }),
+    // db.team.aggregate({ _count: true }),
+  ]);
+  console.log(res);
+};
 
-main()
+main();
