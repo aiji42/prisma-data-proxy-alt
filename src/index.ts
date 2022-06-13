feb8634209b990f75cd55884cca0983220dff2b7
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-06-13 12:51:22 +0900

    custom error handler

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from "apollo-server-express";
 import { PrismaClient, Prisma } from "@prisma/client";
 import { makeTypeDefs } from "./helpers/makeTypeDefs";
 import { makeResolver } from "./helpers/makeResolver";
+import { customError } from "./helpers/customError";
 
 const db = new PrismaClient({
   log: ["query", "info", "warn", "error"],
@@ -19,6 +20,7 @@ const corsOrigin = process.env.CORS_ORIGIN;
   });
 
   await Promise.all([db.$connect(), server.start()]);
+  app.use(customError);
   server.applyMiddleware({ app, path: "/*" });
   app.listen({ port }, () =>
     console.log(
