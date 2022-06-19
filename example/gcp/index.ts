2dec9a40cca4a473b08563880ff346cfdd744581
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-06-19 17:58:40 +0900

    chore: update gcp example

--- example/gcp/index.ts
+++ example/gcp/index.ts
@@ -8,24 +8,28 @@ import {
 } from "prisma-data-proxy-alt";
 
 const db = new PrismaClient();
+db.$connect();
 
-const port = process.env.PORT || "3000";
 const apiKey = process.env.DATA_PROXY_API_KEY || "foo";
 
-(async () => {
-  const app = express();
-  const server = new ApolloServer(makeServerConfig(Prisma, db));
+const app = express();
+app.use(beforeMiddleware({ apiKey }));
+app.use(afterMiddleware());
 
-  await Promise.all([db.$connect(), server.start()]);
-  app.use(beforeMiddleware({ apiKey }));
-  app.use(afterMiddleware());
+const server = new ApolloServer({
+  ...makeServerConfig(Prisma, db),
+});
+
+(async () => {
+  await server.start();
+
   server.applyMiddleware({
     app,
     path: "/*",
   });
-  app.listen({ port }, () =>
-    console.log(
-      `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
-    )
-  );
+  if (process.env.PORT) {
+    app.listen({ port: process.env.PORT });
+  }
 })();
+
+export default app;
