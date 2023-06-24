74e1169430a0b9b904858f3d2ee13a26a05b6fdf
zsnmwy <35299017+zsnmwy@users.noreply.github.com>
2023-06-24 19:33:38 +0000

    chore: make the Test CI Pass

--- src/middlewares/beforeMiddleware.ts
+++ src/middlewares/beforeMiddleware.ts
@@ -3,11 +3,9 @@ import { Request, Response, NextFunction } from "express";
 export const beforeMiddleware =
   ({ apiKey }: { apiKey: string }) =>
   (req: Request, res: Response, next: NextFunction) => {
-    // Bypass Metrics
-    if (req.baseUrl === "/")
-      if (req.method === "POST")
-        // support request by @prisma/client/edge
-        req.headers["content-type"] = "application/json";
+    if (req.method === "POST")
+      // support request by @prisma/client/edge
+      req.headers["content-type"] = "application/json";
 
     // auth check
     const token = req.headers.authorization || "";
